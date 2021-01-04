58384b9d1957457b01443d33ff2cec649ef905c9
wanding-sai <56376522+wanding-sai@users.noreply.github.com>
2021-01-04 18:55:36 +0800

    Add files via upload

---
+++ ScreenRecorder.cs
@@ -0,0 +1,191 @@
+﻿using NLog;
+using ScreenRecorderLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NotepadTest
+{
+    public class ScreenRecorder
+    {
+        private Recorder recorder;
+        private bool isRecording;
+        private bool isCompleted;
+        private string videoPath;
+        private ScreenRecordStatus status;
+        private string error = string.Empty;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private ScreenRecorder(string videopath)
+        {
+            recorder = Recorder.CreateRecorder();
+            recorder.OnRecordingComplete += Recorder_OnRecordingComplete;
+            recorder.OnRecordingFailed += Recorder_OnRecordingFailed;
+            recorder.OnStatusChanged += Recorder_OnStatusChanged;
+            this.videoPath = videopath;
+        }
+
+        /// <summary>
+        /// Create screen record object
+        /// </summary>
+        /// <param name="videopath">Video file saved path</param>
+        /// <returns>ScreenRecorder object</returns>
+        public static ScreenRecorder CreateRecorder(string videopath)
+        {
+            if (File.Exists(videopath))
+            {
+                File.Delete(videopath);
+            }
+
+            ScreenRecorder screenRecorder = new ScreenRecorder(videopath);
+            return screenRecorder;
+        }
+
+        /// <summary>
+        /// Status of recording
+        /// </summary>
+        public ScreenRecordStatus Status
+        {
+            get
+            {
+                return this.status;
+            }
+        }
+        /// <summary>
+        /// Error of recording
+        /// </summary>
+        public string Error
+        {
+            get
+            {
+                return this.error;
+            }
+        }
+
+        /// <summary>
+        /// Is completed
+        /// </summary>
+        public bool IsCompleted
+        {
+            get
+            {
+                return this.isCompleted;
+            }
+        }
+
+        /// <summary>
+        /// Start recording
+        /// </summary>
+        public void StartRecording()
+        {
+            recorder.Record(this.videoPath);
+            int time = 0;
+            do
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                time++;
+                if (time >= 180)
+                {
+                    break;
+                }
+            }
+            while (this.Status != ScreenRecordStatus.Recording);
+        }
+
+        public void StartRecording(ScreenRecorder screenRecorder, int tryCount = 5)
+        {
+            try
+            {
+                int tryIndex = 1;
+                do
+                {
+                    _logger.Info($"Try Count {tryIndex}: The status of recorder is " + screenRecorder.Status);
+                    if (screenRecorder.Status == ScreenRecordStatus.Fail ||
+                        (screenRecorder.Status == ScreenRecordStatus.Idle && tryIndex > 1))
+                    {
+                        _logger.Info($"Try Count {tryIndex}: Re-recording");
+                        screenRecorder = ScreenRecorder.CreateRecorder(screenRecorder.videoPath);
+                        screenRecorder.StartRecording();
+                    }
+                    else if (screenRecorder.Status != ScreenRecordStatus.Recording)
+                    {
+                        _logger.Info($"Try Count {tryIndex}: Start recording");
+                        screenRecorder.StartRecording();
+                    }
+
+                    _logger.Info($"Try Count {tryIndex}: Now recorder status is " + recorder.Status);
+                    tryIndex++;
+                } while (tryIndex <= tryCount && screenRecorder.Status != ScreenRecordStatus.Recording);
+            }
+            catch (Exception ex)
+            {
+                _logger.Info("Exception occurs when start recording.");
+                _logger.Info(ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// End recording
+        /// </summary>
+        public void EndRecording()
+        {
+            recorder.Stop();
+            // Waiting for 10 minutes max
+            SpinWait.SpinUntil(() => isRecording == false, 600000);
+        }
+
+       
+
+        private void Recorder_OnRecordingComplete(object sender, RecordingCompleteEventArgs e)
+        {
+            isRecording = false;
+            isCompleted = true;
+        }
+
+        private void Recorder_OnRecordingFailed(object sender, RecordingFailedEventArgs e)
+        {
+            this.error = e.Error;
+            this.status = ScreenRecordStatus.Fail;
+            isRecording = false;
+        }
+
+        private void Recorder_OnStatusChanged(object sender, RecordingStatusEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case RecorderStatus.Idle:
+                    this.status = ScreenRecordStatus.Idle;
+                    break;
+                case RecorderStatus.Recording:
+                    isRecording = true;
+                    this.status = ScreenRecordStatus.Recording;
+                    break;
+                case RecorderStatus.Paused:
+                    this.status = ScreenRecordStatus.Paused;
+                    break;
+                case RecorderStatus.Finishing:
+                    this.status = ScreenRecordStatus.Finishing;
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Enum for recording status
+        /// </summary>
+        public enum ScreenRecordStatus : int
+        {
+            Idle = 0,
+            Recording = 1,
+            Paused = 2,
+            Finishing = 3,
+            Fail = -1
+        }
+    }
+}
+
---
+++ WinDriverUtil.cs
@@ -0,0 +1,58 @@
+﻿using NLog;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadTest
+{
+    public class WinDriverUtil
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private static WindowsDriver<WindowsElement> _desktopSession = null;
+        /// <summary>
+        /// Url to communicate with Windows Application Driver
+        /// </summary>
+        public const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+
+        private static uint _remainCount = 0;
+        private const uint _refreshCount = 7;
+        private static readonly TimeSpan _commandTimeOut = new TimeSpan(0, 1, 30);
+        /// <summary>
+        /// Obtain current desktop session
+        /// </summary>
+        public static WindowsDriver<WindowsElement> DesktopSession
+        {
+            get
+            {
+                if (_remainCount == 0)
+                {
+                    // Dispose the old session
+                    _desktopSession?.Dispose();
+                    _remainCount = _refreshCount;
+                    var desktopDesiredCapabilities = new DesiredCapabilities();
+                    desktopDesiredCapabilities.SetCapability("app", "Root");
+                    // Assign new session
+                    _desktopSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl),
+                        desktopDesiredCapabilities, _commandTimeOut);
+                }
+
+                --_remainCount;
+                return _desktopSession;
+            }
+        }
+        /// <summary>
+        /// Shot the session part screen and save it with full file name
+        /// </summary>
+        /// <param name="session">Session you want to shot</param>
+        /// <param name="fullFileName">File name for image</param>
+        public static void ShotScreen(WindowsDriver<WindowsElement> session, string fullFileName)
+        {
+            session.GetScreenshot().SaveAsFile(fullFileName, ScreenshotImageFormat.Png);
+        }
+    }
+}
